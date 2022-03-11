37dc7f256fa2b0144330ac647182114c80e9c7f8
Dragos Popa <dragos27popa@gmail.com>
2022-03-11 15:21:18 +0200

    added ArrayList students

--- main/src/BussinesUBB.java
+++ main/src/BussinesUBB.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class BussinesUBB {
 //     O sa contina o lista de studenti
 //     o sa avem metoda pay
 //
+
+    public static ArrayList<Student> students =new ArrayList<>();
+    
 }
