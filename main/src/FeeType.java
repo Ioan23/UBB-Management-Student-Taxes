dd55c425619bb3c305b983c533b1b4654f8d48d5
Dragos Popa <dragos27popa@gmail.com>
2022-03-11 14:34:41 +0200

    create Student class fee class and fee type enum\

---
+++ main/src/FeeType.java
@@ -0,0 +1,4 @@
+public enum FeeType {
+    ANNUAL,
+    QUARTERLY
+}
--- main/src/Student.java
+++ main/src/Student.java
@@ -1,2 +1,11 @@
 public class Student {
+    private int id;
+    private String name;
+    private int yearStudy;
+
+    public Student (int id, String name, int yearStudy){
+        this.id=id;
+        this.name=name;
+        this.yearStudy=yearStudy;
+    }
 }
