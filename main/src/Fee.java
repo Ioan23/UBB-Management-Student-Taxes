f7f763f364aae6925add97ab4990cf9129b32e22
Dragos Popa <dragos27popa@gmail.com>
2022-03-11 15:05:34 +0200

    remove

--- main/src/Fee.java
+++ main/src/Fee.java
@@ -3,7 +3,6 @@ public class Fee {
     private String currency;
     private double discount;
     private FeeType feeType;
-    private String blabla;
 
 
     public <feeType> Fee(double amount, String currency, double discount, FeeType feeType){
