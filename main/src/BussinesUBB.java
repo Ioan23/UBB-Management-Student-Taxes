4bb49eef70af718afd2683c481de46d6f536d66f
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-03-15 14:45:18 +0200

    Try something

--- main/src/BussinesUBB.java
+++ main/src/BussinesUBB.java
@@ -5,16 +5,12 @@ public class BussinesUBB {
         Student UBBStudent;
         Fee UBBFee;
 
-
-
-//     O sa contina o lista de studenti
-//     o sa avem metoda pay
-//
     List<Student> studentList = new ArrayList<>();
 
-    public void setStudentList(List<Student> studentList) {
-        this.studentList = studentList;
-    }
+
+
+
+
 
 }
 
